31f4d90648f201cd5338901c13d4861de9714ade
Raul Juarez <raul.jrz@gmail.com>
2021-10-21 19:28:47 -0300

    The middleware was added so that it receives the json and converts them into an array

--- src/App/Middleware.php
+++ src/App/Middleware.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Slim\App;
 return function (App $app) {
     $settings = $app->getContainer()->get('settings');
+    $app->addBodyParsingMiddleware();
     $app->addErrorMiddleware(
         $settings['displayErrorDetails'],
         $settings['logErrors'],
